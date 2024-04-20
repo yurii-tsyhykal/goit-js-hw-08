c06aab95c54cd308ee94fe529312343374f992ca
Yurii <y.tsyhykal@gmail.com>
2024-04-21 00:34:29 +0200

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
-function checkForSpam(message) {
-  let toLowerMessage = message.toLowerCase();
-  let checkMessage =
-    toLowerMessage.includes('spam') || toLowerMessage.includes('sale');
-  return checkMessage;
+
+function filterArray(numbers, value) {
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
+    }
+  }
+  return result;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
