55e0d45391bc690cc03d0cdbcbf60bfc524c2086
Yurii <y.tsyhykal@gmail.com>
2024-05-25 22:22:51 +0200

    homework

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const listItem = document.querySelectorAll('.item');
-console.log(`Number of categories: ${listItem.length}`);
-
-const MessageTextContent = listItem.forEach(el => {
-  const title = el.querySelector('h2');
-  console.log(`Category: ${title.textContent}`);
-  const arrayOfCategoriesItems = el.querySelectorAll('li');
-  console.log(`Elements: ${arrayOfCategoriesItems.length}`);
-});
